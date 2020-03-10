13c5ca549ced2980f6437b548785e1e9416c9a60
SabinaSadowska <sadowwwska@gmail.com>
2020-03-10 11:18:05 +0100

    przekazywanie zmiennych

--- src/footer.component.js
+++ src/footer.component.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
 
+const text = "Contrary to popular belief, Lorem Ipsum is not simply random text.It has roots in a piece of classical Latin literature from 45 BC,it over 2000 years old. Richard McClintock, a Latin professor "
+
+
 class Footer extends Component {
     render() {
         return <div>
             <hr></hr>
-            <p>Contrary to popular belief, Lorem Ipsum is not simply random text.
-                It has roots in a piece of classical Latin literature from 45 BC,
-                it over 2000 years old. Richard McClintock, a Latin professor 
+            <p>
+                {text}
             </p>
         </div>
     }
 }
 
-export default Footer
+export default Footer 
