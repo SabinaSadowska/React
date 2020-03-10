073a79516354ca5c4b6bda0c68d9b7916cd23707
SabinaSadowska <sadowwwska@gmail.com>
2020-03-10 10:39:50 +0100

    komponenty funkcyjne

---
+++ src/footer.component.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Footer() {
+    return <div>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <p>Contrary to popular belief, Lorem Ipsum is not simply random text.
+             It has roots in a piece of classical Latin literature from 45 BC,
+            it over 2000 years old. Richard McClintock, a Latin professor 
+        </p>
+    </div>
+}
+
+export default Footer
---
+++ src/header.component.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Header() {
+    return <div>
+        <h1>Lorem Ipsum is simply dummy text of the
+             printing and typesetting industry. Lorem
+            Ipsum has been the industry's standard dummy
+            text ever since the 1500s, when an unknown printer
+            took a galley of type and scrambled it to make a
+        </h1>
+    </div>
+}
+
+export default Header
